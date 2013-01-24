f5e92570f304a3cf522f6fef6def4c4f007374df
Jose Gomez <design@zonabi.com>
2013-01-23 19:58:21 -0600

    share intent branching started (not working)

--- BoredomBlitzer/src/com/boredomblitzer/boredomblitzer/ShowActivity.java
+++ BoredomBlitzer/src/com/boredomblitzer/boredomblitzer/ShowActivity.java
@@ -1,23 +1,33 @@
 package com.boredomblitzer.boredomblitzer;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ShowActivity extends Activity {
 	
+	String act_txt;
 
-
+	protected static final String TAG = "ShowActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show);
 		
 		Intent intent = getIntent();
-		String act_txt = intent.getStringExtra(MainScreen.ACT_TITLE);
+		act_txt = intent.getStringExtra(MainScreen.ACT_TITLE);
 		String cat_id = intent.getStringExtra(MainScreen.CAT_ID);
 		String cat_txt = intent.getStringExtra(MainScreen.CAT_TITLE);
 		String cat_img = intent.getStringExtra(MainScreen.CAT_IMAGE);
@@ -25,6 +35,55 @@ public class ShowActivity extends Activity {
 		
 		setActTextField(act_txt, cat_txt, cat_id);
 		
+		/*
+		ImageButton sharingButton = new ImageButton(this);
+		sharingButton.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		sharingButton.setImageResource(R.drawable.bblitzer_sharebtn);
+		
+		sharingButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			shareIt();
+			}
+			});
+			*/
+
+		
+	}
+	
+	public void shareIt(View view){
+		//sharing implementation
+		Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+		sharingIntent.setType("text/plain");
+		String shareBody = "Boredom Blitzer suggested to " + act_txt + " to cure boredom! Get the app at http://www.BoredomBlitzer.com";
+		
+		//sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+		//sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "http://www.boredomblitzer.com");
+		PackageManager pm = view.getContext().getPackageManager();
+		List<ResolveInfo> activityList = pm.queryIntentActivities(sharingIntent, 0);
+		for(final ResolveInfo app : activityList) {
+			 Log.i(TAG, "app.actinfo.name: " + app.activityInfo.name);
+			//if((app.activityInfo.name).contains("facebook")) {
+			if("com.facebook.katana.ShareLinkActivity".equals(app.activityInfo.name)) {
+				/*
+				final ActivityInfo activity = app.activityInfo;
+				final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
+				sharingIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+				sharingIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+				sharingIntent.setComponent(name);
+				*/
+				
+				sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "http://www.boredomblitzer.com");
+				startActivity(Intent.createChooser(sharingIntent, "Share idea"));
+				break;
+			} else {
+				sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Boredom Blitzer");
+				sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+				startActivity(Intent.createChooser(sharingIntent, "Share idea"));
+				break;
+			}
+		}
+		
+		//startActivity(Intent.createChooser(sharingIntent, "Share idea"));
 	}
 
 	@Override
@@ -33,6 +92,8 @@ public class ShowActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_show, menu);
 		return true;
 	}
+	
+	
 	
 	public void setActTextField(String txtStr, String txtCat, String intCat){
 		//TODO something
@@ -112,5 +173,7 @@ public class ShowActivity extends Activity {
 				break;	
 		}
 	}
+	
+	
 
 }
