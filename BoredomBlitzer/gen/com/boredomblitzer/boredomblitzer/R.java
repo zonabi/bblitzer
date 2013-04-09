00e3f02eb9a2f65fb2db2751e582eb6db2c087d1
zonabi <zonabidesign@gmail.com>
2013-04-09 09:03:37 -0500

    updated app icon, fixed some strings

--- BoredomBlitzer/gen/com/boredomblitzer/boredomblitzer/R.java
+++ BoredomBlitzer/gen/com/boredomblitzer/boredomblitzer/R.java
@@ -63,19 +63,20 @@ public final class R {
         public static final int activity_show=0x7f060002;
     }
     public static final class string {
-        public static final int about_main=0x7f040007;
-        public static final int about_top=0x7f040006;
-        public static final int activity_bottom_txt=0x7f04000b;
-        public static final int activity_category_txt=0x7f04000a;
-        public static final int activity_top_txt=0x7f040009;
+        public static final int about_main=0x7f040008;
+        public static final int about_top=0x7f040007;
+        public static final int activity_bottom_txt=0x7f04000c;
+        public static final int activity_category_txt=0x7f04000b;
+        public static final int activity_top_txt=0x7f04000a;
         public static final int app_name=0x7f040000;
-        public static final int blitzBtnDesc=0x7f040004;
-        public static final int hello_world=0x7f040005;
-        public static final int main_screen_bottom=0x7f040002;
-        public static final int main_screen_msg=0x7f040001;
-        public static final int menu_settings=0x7f040003;
-        public static final int title_activity_about_app=0x7f04000c;
-        public static final int title_activity_show=0x7f040008;
+        public static final int blitzBtnDesc=0x7f040005;
+        public static final int hello_world=0x7f040006;
+        public static final int main_screen_bottom=0x7f040003;
+        public static final int main_screen_msg=0x7f040002;
+        public static final int main_screen_name=0x7f040001;
+        public static final int menu_settings=0x7f040004;
+        public static final int title_activity_about_app=0x7f04000d;
+        public static final int title_activity_show=0x7f040009;
     }
     public static final class style {
         /** 
